7bc87403553146da3bf381ea6bf2542409888d12
Jacob Odden <jacob.l.odden@gmail.com>
2019-01-29 20:00:42 -0500

    More progress, about half way through chapter

--- src/App.js
+++ src/App.js
@@ -17,14 +17,55 @@ const list = [
     num_comments: 2,
     points: 5,
     objectID: 1
+  },
+  {
+    title: 'Redux 2',
+    url: 'https://redux.js.org/',
+    author: 'Jacob',
+    num_comments: 4,
+    points: 3,
+    objectID: 2
   }
 ]
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list,
+      searchTerm: ''
+    }
+
+    this.onDissmiss = this.onDissmiss.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+  }
+
+  onDissmiss(id) {
+    const isNotID = item => item.objectID !== id
+    const updatedList = this.state.list.filter(isNotID)
+    this.setState({ list: updatedList })
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
+    const { searchTerm, list } = this.state
     return (
       <div className="App">
-        {list.map(item => {
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+            value={searchTerm}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item => {
           return (
             <div key={item.objectID}>
               <span>
@@ -33,6 +74,14 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDissmiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
             </div>
           )
         })}
